70516bb0603e4a0209b74e4505e9a1bbfefca66e
Bit Handshake <adam@monotech.hu>
2023-06-07 11:12:10 +0200

    0.3.3.0

--- source-code/cljc/metamorphic_content/core.cljs
+++ source-code/cljc/metamorphic_content/core.cljs
@@ -19,6 +19,9 @@
   ;   W/ {:content (keyword or string)}
   ;  :suffix (string)(opt)
   ;   W/ {:content (keyword, number or string)}}
+  ; @param (metamorphic-content)(opt) placeholder-props
+  ;  In case of the composed content is empty the function composes the placeholder
+  ;  content instead.
   ;
   ; @usage
   ; (compose {...})
@@ -59,95 +62,106 @@
   ; (defn my-component [color])
   ; [compose {:content [my-component]
   ;           :params  [:green]}]
-  [content-props]
-  (letfn [
-          ; (string-content {:content "Hi, my name is %!" :replacements ["John"]})
-          ; =>
-          ; "Hi, my name is John!"
-          (string-content [content-props]
-            (utils/join-content content-props))
+  ([content-props]
+   (compose content-props nil))
 
-          ; (number-content {:content 42})
-          ; =>
-          ; "42"
-          ;
-          ; (number-content {:content 42 :suffix "kg"})
-          ; =>
-          ; "42kg"
-          (number-content [content-props]
-            (string-content (update content-props :content str)))
+  ([content-props placeholder-props]
+   (letfn [
+           ; (string-content {:content "Hi, my name is %!" :replacements ["John"]})
+           ; =>
+           ; "Hi, my name is John!"
+           (string-content [content-props]
+             (utils/join-content content-props))
 
-          ; (dictionary-content {:content :apple})
-          ; =>
-          ; "Apple"
-          ;
-          ; (dictionary-content {:content :hi-my-name-is-n :replacements ["John"]})
-          ; =>
-          ; "Hi, my name is John!"
-          (dictionary-content [content-props]
-            (string-content (update content-props :content dictionary/look-up)))
+           ; (number-content {:content 42})
+           ; =>
+           ; "42"
+           ;
+           ; (number-content {:content 42 :suffix "kg"})
+           ; =>
+           ; "42kg"
+           (number-content [content-props]
+             (string-content (update content-props :content str)))
 
-          ; (hiccup-content {:content [:div "Apple"]})
-          ; =>
-          ; [:div "Apple"]
-          (hiccup-content [content-props]
-            (:content content-props))
+           ; (dictionary-content {:content :apple})
+           ; =>
+           ; "Apple"
+           ;
+           ; (dictionary-content {:content :hi-my-name-is-n :replacements ["John"]})
+           ; =>
+           ; "Hi, my name is John!"
+           (dictionary-content [content-props]
+             (string-content (update content-props :content dictionary/look-up)))
 
-          ; (defn my-component [] ...)
-          ; (render-fn-content {:content my-component})
-          ; =>
-          ; [my-component]
-          ;
-          ; (defn my-component [] ...)
-          ; (render-fn-content {:content #'my-component})
-          ; =>
-          ; [my-component]
-          ;
-          ; (defn my-component [my-param] ...)
-          ; (render-fn-content {:content #'my-component :params ["My value"]})
-          ; =>
-          ; [my-component "My-value"]
-          (render-fn-content [{:keys [content params]}]
-            (if params (vector/concat-items [content] params)
-                       [content]))
+           ; (hiccup-content {:content [:div "Apple"]})
+           ; =>
+           ; [:div "Apple"]
+           (hiccup-content [content-props]
+             (:content content-props))
 
-          ; (defn my-component [] ...)
-          ; (component-content {:content [my-component]})
-          ; =>
-          ; [my-component]
-          ;
-          ; (defn my-component [my-param] ...)
-          ; (component-content {:content [my-component] :params ["My value"]})
-          ; =>
-          ; [my-component "My-value"]
-          (component-content [{:keys [content params]}]
-            (if params (vector/concat-items content params)
-                       (return content)))
+           ; (defn my-component [] ...)
+           ; (render-fn-content {:content my-component})
+           ; =>
+           ; [my-component]
+           ;
+           ; (defn my-component [] ...)
+           ; (render-fn-content {:content #'my-component})
+           ; =>
+           ; [my-component]
+           ;
+           ; (defn my-component [my-param] ...)
+           ; (render-fn-content {:content #'my-component :params ["My value"]})
+           ; =>
+           ; [my-component "My-value"]
+           (render-fn-content [{:keys [content params]}]
+             (if params (vector/concat-items [content] params)
+                        [content]))
+
+           ; (defn my-component [] ...)
+           ; (component-content {:content [my-component]})
+           ; =>
+           ; [my-component]
+           ;
+           ; (defn my-component [my-param] ...)
+           ; (component-content {:content [my-component] :params ["My value"]})
+           ; =>
+           ; [my-component "My-value"]
+           (component-content [{:keys [content params]}]
+             (if params (vector/concat-items content params)
+                        (return content)))
+
+           ; ...
+           (metamorphic-content [{:keys [content] :as content-props}]
+             ; #' The symbol must resolve to a var, and the Var object itself (not its value) is returned.
+             ;
+             ; (var? #'my-component) => true
+             ; (fn?  #'my-component) => true
+             ; (var?   my-component) => false
+             ; (fn?    my-component) => true
+             ;
+             ; The fn? function matches with both types (#'my-component, my-component).
+             (cond (keyword?   content) (dictionary-content content-props)
+                   (string?    content) (string-content     content-props)
+                   (number?    content) (number-content     content-props)
+                 ; (var?       content) [render-fn-content  content-props]
+                   (fn?        content) [render-fn-content  content-props]
+                   (component? content) [component-content  content-props]
+                   (hiccup?    content) (hiccup-content     content-props)
+                   :return     content))
+
+           ; You can pass the content as a map that contains the :content key and other
+           ; parameters or as a shorthand format that will be converted to a map.
+           ;
+           ; [component {:content "Apple"}] <= Default format
+           ; [component "Apple"]            <= Shorthand format
+           (use-shorthand-f [content-props]
+             (if (map?     content-props)
+                 (return   content-props)
+                 {:content content-props}))]
 
           ; ...
-          (metamorphic-component [{:keys [content] :as content-props}]
-            ; #' The symbol must resolve to a var, and the Var object itself (not its value) is returned.
-            ;
-            ; (var? #'my-component) => true
-            ; (fn?  #'my-component) => true
-            ; (var?   my-component) => false
-            ; (fn?    my-component) => true
-            ;
-            ; The fn? function matches with both types (#'my-component, my-component).
-            (cond (keyword?   content) (dictionary-content content-props)
-                  (string?    content) (string-content     content-props)
-                  (number?    content) (number-content     content-props)
-                ; (var?       content) [render-fn-content  content-props]
-                  (fn?        content) [render-fn-content  content-props]
-                  (component? content) [component-content  content-props]
-                  (hiccup?    content) (hiccup-content     content-props)
-                  :return     content))]
-
-         ; You can pass the content as a map that contains the :content key and other
-         ; parameters or as a shorthand format that will be converted to a map.
-         ;
-         ; [component {:content "Apple"}] <= Default format
-         ; [component "Apple"]            <= Shorthand format
-         (if (map? content-props)
-             (metamorphic-component           content-props)
-             (metamorphic-component {:content content-props}))))
+          (let [content-props    (use-shorthand-f     content-props)
+                composed-content (metamorphic-content content-props)]
+               (if-not (empty? composed-content)
+                       (return composed-content)
+                       (if placeholder-props (compose placeholder-props)))))))
